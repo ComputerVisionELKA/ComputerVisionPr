4bf8c571239f860e541ad56ac38a7801e3c956b5
CyrusVorwald <vgt085@gmail.com>
2015-07-20 14:17:07 -0400

    k

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+
+#opencv
+OPENCVROOT:= /Users/Cyrus/Android/OpenCV-android-sdk
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=SHARED
+include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
+
+
+LOCAL_SRC_FILES := main.cpp
+LOCAL_LDLIBS += -llog
+LOCAL_MODULE := features
+
+include $(BUILD_SHARED_LIBRARY)
